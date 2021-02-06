7bd234f1a569ea4ef68fc845e029bdec8df5cb18
Mandeep Rooprai <mandeep.rooprai@kcl.ac.uk>
2021-02-06 09:37:33 +0000

    First commit of files

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({ title }) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS - for dynamic styling
+// Create style variable
+// const headingStyle = {
+//     color: 'red',
+//     backgroundColor: 'black',
+// }
+
+export default Header
