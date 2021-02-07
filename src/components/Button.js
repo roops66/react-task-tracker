532c336464a90d4c8c1bfd36caade9e0f1e7fe9a
Mandeep Rooprai <mandeep.rooprai@kcl.ac.uk>
2021-02-07 00:02:27 +0000

    Added some State and Task/s components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,21 @@
-const Button = ({ color, text }) => {
+import PropTypes from 'prop-types'
+
+const Button = ({ color, text, onClick }) => {
+
+
     return (
-    <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+        <button onClick={onClick} style={{ backgroundColor: color }} className='btn'>{text}</button>
     )
+}
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
 }
 
 export default Button
